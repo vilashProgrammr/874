2302e6ad91c644718933ac2be0b3cd2484be2daf
candidate <candidate@programmr.com>
2019-09-23 07:29:16 +0000

    FINISH TEST

--- src/component/ButtonPanel.js
+++ src/component/ButtonPanel.js
@@ -23,7 +23,7 @@ export default class ButtonPanel extends React.Component {
           <Button name="÷" clickHandler={this.handleClick} orange />
         </div>
         <div>
-          <Button name="7" clickHandler={this.handleClick} />
+          <Button name="117" clickHandler={this.handleClick} />
           <Button name="8" clickHandler={this.handleClick} />
           <Button name="9" clickHandler={this.handleClick} />
           <Button name="x" clickHandler={this.handleClick} orange />
